c278b654aad18bc2b3d8e34028d20dd5a6dea202
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-08 17:33:14 -0300

    Resolução do exercício

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// Faça um programa que exiba uma contagem de 0 a 10 na mesma linha, aguardando um segundo entre cada número.
+
+Console.Write ("Contando até Dez: 0,");
+Thread.Sleep (1000);
+Console.Write (" 1," );
+Thread.Sleep (1000);
+Console.Write (" 2,");
+Thread.Sleep (1000);
+Console.Write (" 3,");
+Thread.Sleep (1000);
+Console.Write (" 4,");
+Thread.Sleep (1000);
+Console.Write (" 5,");
+Thread.Sleep (1000);
+Console.Write (" 6,");
+Thread.Sleep (1000);
+Console.Write (" 7,");
+Thread.Sleep (1000);
+Console.Write (" 8,");
+Thread.Sleep (1000);
+Console.Write (" 9,");
+Thread.Sleep (1000);
+Console.WriteLine (" 10");
+Console.WriteLine();
+Console.ReadLine();
+Console.Clear();
